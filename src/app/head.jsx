5d3f6cadd567f0ba175e6ee248c5ed6beef05c7c
kevin <kevincruzalejo172@gmail.com>
2023-04-25 13:35:46 -0500

    version 0.01

--- src/app/head.jsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <html>
-      <title>kacs-webside</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="" />
-    </html>
-  )
-}
